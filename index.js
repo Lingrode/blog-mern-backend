a7c67f3cc8176bf56130c7cbc53de12be7b25575
Lingrode <grimaloilya@gmail.com>
2024-06-20 23:19:03 -0600

    added method create in PostController

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 
-import { registerValidation, loginValidation } from "./validations.js";
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
 
 import checkAuth from "./utils/checkAuth.js";
 
 import * as UserController from "./controllers/UserController.js";
+import * as PostController from "./controllers/PostController.js";
 
 mongoose
   .connect(
@@ -21,6 +26,12 @@ app.use(express.json());
 app.post("/auth/register", registerValidation, UserController.register);
 app.post("/auth/login", loginValidation, UserController.login);
 app.get("/auth/me", checkAuth, UserController.getMe);
+
+// app.get("/post", PostController.getAll);
+// app.get("/post/:id", PostController.getOne);
+app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+// app.delete("/posts", PostController.remove);
+// app.patch("/posts", PostController.update);
 
 app.listen(4444, (err) => {
   if (err) {
